e40bae6471f097e733f0b2ebffb4d9f665d2f6f1
Emily Reyes <emilymwind1@gmail.com>
2023-04-12 10:49:59 -0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using onnx.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace onnx.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly OnnxModelScorer _scorer;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+
+            // Load the ONNX model
+            var modelPath = "C:/Users/emily/source/repos/onnx/supervised.onnx";
+            _scorer = new OnnxModelScorer(modelPath);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public IActionResult Predict(IFormCollection form)
+        {
+            // Get the form data
+            float[] inputValues = new float[15];
+            for (int i = 0; i < 15; i++)
+            {
+                if (!float.TryParse(form[$"input_{i}"], out inputValues[i]))
+                {
+                    return BadRequest("Invalid input value");
+                }
+            }
+
+            // Score the inputs
+            float[] outputValues = _scorer.Score(inputValues);
+
+            // Return the output as a JSON object
+            var output = new { values = outputValues };
+            return Json(output);
+        }
+    }
+}
---
+++ Models/OnnxModelScorer.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
+using System;
+
+namespace onnx.Models
+{
+    public class OnnxModelScorer : IDisposable
+    {
+        private readonly InferenceSession _session;
+        private readonly string _inputName;
+        private readonly int[] _inputShape;
+        private readonly int[] _outputShape;
+
+        public OnnxModelScorer(string modelPath)
+        {
+            _session = new InferenceSession(modelPath);
+
+            // Get the input and output metadata
+            _inputName = _session.InputMetadata.Keys.First();
+            _inputShape = _session.InputMetadata[_inputName].Dimensions.ToArray();
+            var outputName = _session.OutputMetadata.Keys.First();
+            _outputShape = _session.OutputMetadata[outputName].Dimensions.ToArray();
+        }
+
+        public float[] Score(float[] inputs)
+        {
+            // Create the input tensor
+            var tensor = new DenseTensor<float>(new[] { 1, _inputShape[1] }, inputs);
+
+            // Run the prediction
+            var inputsList = new List<NamedOnnxValue> { NamedOnnxValue.CreateFromTensor<float>(_inputName, tensor) };
+            using (var results = _session.Run(inputsList))
+            {
+                // Get the output tensor
+                var outputTensor = results.First().AsTensor<float>();
+
+                // Extract the output values
+                float[] outputValues = new float[_outputShape[1]];
+                for (int i = 0; i < _outputShape[1]; i++)
+                {
+                    outputValues[i] = outputTensor[0, i];
+                }
+
+                return outputValues;
+            }
+        }
+
+        public void Dispose()
+        {
+            _session.Dispose();
+        }
+    }
+}
